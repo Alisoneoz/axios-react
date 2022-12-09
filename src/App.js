ccb73297c823e7dc5caa38698774bdaada126fcf
Alison Estephany <alisonestephanyoz@gmail.com>
2022-12-09 17:26:49 -0400

    feat: add axios and fetch data from official joke api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
+
 
 function App() {
+
+  const getJoke = ()=> {
+  Axios.get('https://official-joke-api.appspot.com/random_joke')
+    .then((response) => {
+      console.log(response)
+    })
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Wanna read the Joke of the day?</h1>
+        <button onClick={getJoke}>Click Me</button>
+      </div>
     </div>
   );
 }
