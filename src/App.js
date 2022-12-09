f5148bfaeeae9e66cf02576e9c6f95a1786720fe
Alison Estephany <alisonestephanyoz@gmail.com>
2022-12-09 18:53:48 -0400

    style: add style

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import './App.css';
 import Axios from "axios";
-
+import { useState } from 'react';
 
 function App() {
+  const [joke, setJoke]= useState([]);
 
   const getJoke = ()=> {
   Axios.get('https://official-joke-api.appspot.com/random_joke')
     .then((response) => {
-      console.log(response)
+      setJoke(`${response.data.setup} ...  ${response.data.punchline}`)
     })
 }
 
+console.log(joke)
+
   return (
     <div className="App">
+      
       <div>
         <h1>Wanna read the Joke of the day?</h1>
-        <button onClick={getJoke}>Click Me</button>
+        
+        <div className='btn'>
+          <button onClick={getJoke}>Click Me</button>
+        </div>
+        
+        <br />
+        <div className='joke'>
+          {joke}
+        </div>
+        
       </div>
     </div>
   );
